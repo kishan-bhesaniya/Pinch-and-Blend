3ae6e4c3e92a3b6d9deee51e61bb932939d2a9d9
kishan-bhesaniya <kishanbhesaniya283@gmail.com>
2026-07-29 13:46:14 +0530

    fix: mobile responsiveness

--- js/swiper.js
+++ js/swiper.js
@@ -6,4 +6,29 @@ let swiper = new Swiper(".testi-swiper", {
     el: ".swiper-pagination",
     clickable: true,
   },
+  breakpoints: {
+    200: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+      centeredSlides: false,
+    },
+    767: {
+      slidesPerView: 1.5,
+      spaceBetween: 40,
+      centeredSlides: true,
+    },
+    991: {
+      spaceBetween: 50,
+    },
+    1199: {
+      slidesPerView: 1.8,
+      spaceBetween: 50,
+    },
+  },
+});
+
+$(document).ready(function () {
+  $(".navbar-toggler").click(function () {
+    $(".collapse").addClass("show");
+  });
 });
